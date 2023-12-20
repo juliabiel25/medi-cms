fd85261cbf42aa7b4e222fec130fc428b6e9b094
pgrochowska <pauline.grochowska@gmail.com>
2023-12-20 14:44:04 +0100

    New frontend 2

--- frontpage/src/pages/Doctors.tsx
+++ frontpage/src/pages/Doctors.tsx
@@ -1,20 +1,15 @@
-import About from '../components/About';
-import AppointmentForm from '../components/AppointmentForm';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Home from '../components/Home';
 import Map from '../components/Map';
-import News from '../components/News';
 import Spinner from '../components/Spinner';
-import Team from '../components/Team';
+import TeamHorizontal from '../components/TeamHorizontal';
 
 const DoctorsPage = () => {
   return (
     <div id="doctors-page">
       <Spinner />
       <Header />
-      <Home />
-      <About />
+      <TeamHorizontal/>
       <Map />
       <Footer />
     </div>
